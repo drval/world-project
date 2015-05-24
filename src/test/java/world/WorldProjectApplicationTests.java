6509d82b562510544e70e83d191bb5af710a0dff
drval <rudrval@gmail.com>
2015-05-24 18:56:44 +0400

    simple selenium test added

--- src/test/java/world/WorldProjectApplicationTests.java
+++ src/test/java/world/WorldProjectApplicationTests.java
@@ -1,7 +1,11 @@
 package world;
 
+import junit.framework.TestCase;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -9,10 +13,18 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = WorldProjectApplication.class)
 @WebAppConfiguration
-public class WorldProjectApplicationTests {
+public class WorldProjectApplicationTests extends TestCase {
 
 	@Test
 	public void contextLoads() {
 	}
-
+	
+	@Test
+	public void testIndexPage() {
+		System.setProperty("webdriver.chrome.driver", "C:\\dev\\chromedriver.exe");
+		ChromeDriver driver = new ChromeDriver();		
+		driver.get("http://localhost:8084");
+		assertEquals(driver.findElementById("title").getText(), "World-project using spring boot and Angular");
+		driver.quit();
+	}
 }
